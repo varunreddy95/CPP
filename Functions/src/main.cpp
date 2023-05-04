d98a5fb2ea323a7cdf23ba2cb0d71d4d0719691b
varunreddy95 <varunreddy_95@outlook.com>
2023-05-05 00:20:30 +0200

    Function definition

--- Functions/src/main.cpp
+++ Functions/src/main.cpp
@@ -3,6 +3,46 @@
 #include <cstdlib>   //required for rand(), for precise distributions use <random> header file
 #include <ctime>  // required for time()
 
+
+double cal_area_circle(double radius)
+{
+    const double pi {3.14159};
+    return (pi * radius * radius);
+}
+
+
+
+void area_circle()
+{
+    double radius {};
+    std::cout << "Enter the radius of the circle: ";
+    std::cin >> radius;
+
+    std::cout << "The area of a circle with radius " << " is " << cal_area_circle(radius) << std::endl;
+}
+
+
+double calc_volume_cylinder (double radius, double height)
+{
+    return (cal_area_circle(radius) * height);
+}
+
+
+
+void volume_cylinder()
+{
+    double radius {};
+    double height {};
+    
+    std::cout << "Enter the radius of the cylinder: ";
+    std::cin >> radius;
+
+    std::cout << "Enter the height of the cylinder: ";
+    std::cin >> height;
+
+    std::cout << "The area of cylinder with radius " << radius << " and " << " height " << height << " is: " << calc_volume_cylinder(radius, height) << std::endl;
+}
+
 int main()
 {
     // double num {0};
@@ -25,21 +65,32 @@ int main()
     // std::cin >> power;
     // std::cout << num << " raised to the " << power << " power is " << pow(num, power) << std::endl;
 
-    int random_number {0};
-    size_t count {10};  //Number of random numbers to generate
-    int min {1}; // Lower bound inclusive
-    int max {6}; //upper bound inclusive
+    // int random_number {0};
+    // size_t count {10};  //Number of random numbers to generate
+    // int min {1}; // Lower bound inclusive
+    // int max {6}; //upper bound inclusive
 
-    //seed the random number generator
-    std::cout << "RAND_MAX on my system is: " << RAND_MAX << std::endl;
-    srand(time(nullptr));
+    // //seed the random number generator
+    // std::cout << "RAND_MAX on my system is: " << RAND_MAX << std::endl;
+    // srand(time(nullptr));
 
-    for(size_t i=1; i <= count; ++i)
-    {
-        random_number = rand() % max + min;
-        std::cout << random_number << std::endl;
-    }
+    // for(size_t i=1; i <= count; ++i)
+    // {
+    //     random_number = rand() % max + min;
+    //     std::cout << random_number << std::endl;
+    // }
 
+    // std::cout << std::endl;
+
+
+    //functions calls
+
+    area_circle();
+    
     std::cout << std::endl;
+
+    volume_cylinder();
+
+
     return 0;
 }
